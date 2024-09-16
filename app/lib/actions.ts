c1bbd137e86e8263132c4430c47a0dce31d045ce
dslevi <dslevi12@gmail.com>
2024-09-15 21:08:21 -0700

    Add authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { AuthError } from 'next-auth';
+import { signIn } from '@/auth';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -96,7 +98,6 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error("FAiled to delete invoice");
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
     } catch (error) {
@@ -105,4 +106,24 @@ export async function deleteInvoice(id: string) {
         };
     }
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    }
+    catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
